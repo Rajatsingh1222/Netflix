3aeaacd942c79b9bb44420f490b47c27d8711afe
Rajat singh <singhrajat176@gmail.com>
2025-03-22 11:07:04 +0530

    padding and margin in input box in login

--- src/components/Login.js
+++ src/components/Login.js
@@ -79,10 +79,10 @@ dispatch(addUser({uid:uid,email:email,displayName:displayName}));
     <div className='relative'> 
     <img className='h-[700px]' src="https://assets.nflxext.com/ffe/siteui/vlv3/0cf2c109-3af1-4a9d-87d7-aecfac5fe881/web/IN-en-20250217-TRIFECTA-perspective_c3376e06-9aff-4657-aafb-91256a597b7c_large.jpg" alt='logo'/>
     <div className='h-[200px] bg-black'></div>
-    <form onSubmit={(e)=>e.preventDefault()} className='p-8 md:p-12 bg-black  absolute bg-opacity-85 top-[220px] left-[60px] md:left-[500px] w-8/12 md:w-3/12 h-[400px] md:h-[510px]'>
-        {(!isSignIn)&&<input ref={name} type="text" placeholder='Name' className='mb-6 ml-4 mt-6 placeholder-gray-500 p-1 md:p-2 md:m-5 md:ml-1 w-10/12 md:w-full rounded-sm text-gray-800 font-semibold text-sm'></input>}
-        <input ref={email} type="text" placeholder='Email' className='mb-6 ml-4 placeholder-gray-500 p-1 md:p-2 md:m-5 md:ml-1 w-10/12 md:w-full rounded-sm text-gray-800 font-semibold text-sm'></input>
-        <input ref={password} type="text" placeholder='password' className='mb-4 ml-4 placeholder-gray-500 p-1 md:p-2 md:m-5 md:ml-1 w-10/12 md:w-full rounded-sm text-gray-800 font-semibold text-sm'></input>
+    <form onSubmit={(e)=>e.preventDefault()} className='pb-32 p-8 md:p-12 bg-black  absolute bg-opacity-85 top-[220px] left-[60px] md:left-[500px] w-8/12 md:w-3/12 h-[400px] md:h-[510px]'>
+        {(!isSignIn)&&<input ref={name} type="text" placeholder='Name' className='mb-1 ml-4 mt-6 placeholder-gray-500 p-1 md:p-2 md:m-5 md:ml-1 w-10/12 md:w-full rounded-sm text-gray-800 font-semibold text-sm'></input>}
+        <input ref={email} type="text" placeholder='Email' className='mb-6 ml-4 mt-6 placeholder-gray-500 p-1 md:p-2 md:m-5 md:ml-1 w-10/12 md:w-full rounded-sm text-gray-800 font-semibold text-sm'></input>
+        <input ref={password} type="text" placeholder='password' className='mb-4 ml-4 mt-1 placeholder-gray-500 p-1 md:p-2 md:m-5 md:ml-1 w-10/12 md:w-full rounded-sm text-gray-800 font-semibold text-sm'></input>
         <p className='my-2 px-2 text-red-700 font-bold'> {errorMessage}</p>
         <button    className ='bg-red-800 p-4 m-4 font-semibold text-white py-2 my-6 md:ml-1 w-10/12 md:w-full md:h-12 rounded-md ' onClick={handlevalidation}>{isSignIn?"SIGN IN":"sign up"}</button>
 
